dafe2005e21d00daee24b2d0f49cd746fdd52e10
Ronnie Magatti <ronniemagatti@gmail.com>
2021-09-17 00:06:50 -0600

    Make telescope optional
    Closes #26

--- lua/telescope/_extensions/gotopreview.lua
+++ lua/telescope/_extensions/gotopreview.lua
@@ -1,9 +1,9 @@
 local has_telescope, telescope = pcall(require, "telescope")
 local GotoPreview = require('goto-preview')
-
+local lib = require('goto-preview.lib')
 
 if not has_telescope then
-  error("This plugin requires telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)")
+  lib.logger.debug("Telescope.nvim not found. (https://github.com/nvim-telescope/telescope.nvim)")
 end
 
 return telescope.register_extension {
